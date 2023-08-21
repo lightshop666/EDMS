c98f4ca61418cd7d7076a37ad7d088b3df97e549
huijin0809 <huijin0809@naver.com>
2023-08-21 17:28:56 +0900

    [김희진] 세션정보 부서명 추가, 입사일 String타입으로 포맷팅

--- edms/src/main/java/com/fit/service/LoginService.java
+++ edms/src/main/java/com/fit/service/LoginService.java
@@ -18,8 +18,14 @@ public class LoginService {
     public Map<String, Object> validateUser(String memberId, String memberPw) {
     	
 		Map<String, Object> loginSessionMap = loginMapper.selectEmpForSession(memberId, memberPw);
-log.debug(CC.WOO + "로긴서비스.loginSessionMap :  " + loginSessionMap + CC.RESET);
+		log.debug(CC.WOO + "로긴서비스.loginSessionMap :  " + loginSessionMap + CC.RESET);
 		
+		// DB에서 날짜값을 받아오면 String 타입으로 형변환이 필요합니다.
+		log.debug(CC.WOO + "로긴서비스.loginSessionMap employDate의 형변환 전 타입 :  " + loginSessionMap.get("employDate").getClass() + CC.RESET);
+		java.sql.Date employDate = (java.sql.Date) loginSessionMap.get("employDate");
+		String employDateString = employDate.toString();
+		loginSessionMap.put("employDate", employDateString);
+		log.debug(CC.WOO + "로긴서비스.loginSessionMap employDate의 형변환 후 타입 :  " + loginSessionMap.get("employDate").getClass() + CC.RESET);
 		
 		return loginSessionMap;
     }
