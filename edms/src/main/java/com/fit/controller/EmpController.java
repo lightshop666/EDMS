8a9bb8f7e8006e78f03e8f6ec766276be58fb554
yeeunnnn <133733528+yeeunnnn@users.noreply.github.com>
2023-08-21 21:46:17 +0900

    [김예은] 사원 목록 조회 검색 기능 구현중, 조회 쿼리 수정

--- edms/src/main/java/com/fit/controller/EmpController.java
+++ edms/src/main/java/com/fit/controller/EmpController.java
@@ -307,32 +307,28 @@ public class EmpController {
     }
 	
 	// 사원 목록 폼
-	@GetMapping("/emp/empList")
-	public String empList(Model model,
-	                      @RequestParam(value = "downloadExcel", required = false) String downloadExcel,
-	                      @RequestParam(value = "empNos", required = false) List<Integer> empNos,
-	                      HttpServletResponse response) {
-		// downloadExcel과 empNos 파라미터를 이용하여 엑셀 다운로드 여부 및 사원 번호 목록 확인
-	    if (downloadExcel != null && empNos != null) {
+	@GetMapping("/emp/downloadExcel")
+	public void downloadExcel(@RequestParam(value = "empNos", required = false) List<Integer> empNos,
+	                          HttpServletResponse response) {
+	    // empNos가 제공되었는지 확인하여 엑셀 다운로드를 수행합니다.
+	    if (empNos != null) {
 	        try {
-	        	 // 선택된 사원 정보 조회
+	            // 선택된 사원 정보 조회
 	            List<EmpInfo> selectedEmpList = empService.getSelectedEmpList(empNos);
-	            log.debug(CC.YE + "EmpController Get empList() selectedEmpList: " + selectedEmpList + CC.RESET);
 	            
-	            // HTTP 응답 헤더 설정
-	            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"); // HTTP 응답 헤더를 설정. 엑셀 파일의 형식 중 Office Open XML 형식을 나타내는 MIME 유형
-	            response.setHeader("Content-Disposition", "attachment; filename=selected_employees.xlsx"); // 응답 헤더의 "Content-Disposition" 필드를 설정. 다운로드할 파일의 이름과 함께 첨부 파일로 다운로드하도록 지시.
+	            // HTTP 응답 헤더 설정: 다운로드할 엑셀 파일의 형식과 이름을 설정합니다.
+	            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+	            response.setHeader("Content-Disposition", "attachment; filename=selected_employees.xlsx");
 	            
 	            // 엑셀 워크북 및 시트 생성
 	            Workbook workbook = new XSSFWorkbook();
 	            Sheet sheet = workbook.createSheet("Selected Employees");
 	            
 	            // 엑셀 헤더 생성
-	            Row headerRow = sheet.createRow(0);
 	            String[] headers = {"사원번호", "사원명", "부서명", "팀명", "직급", "권한", "재직사항", "입사일"};
-	            log.debug(CC.YE + "EmpController Get empList() headers: " + selectedEmpList + CC.RESET);
+	            Row headerRow = sheet.createRow(0);
 	            
-	            // 헤더의 길이만큼 셀을 생성하고 각각의 셀에 String 값을 세팅
+	            // 헤더 셀 생성 및 헤더 데이터 설정
 	            for (int i = 0; i < headers.length; i++) {
 	                Cell cell = headerRow.createCell(i);
 	                cell.setCellValue(headers[i]);
@@ -343,6 +339,7 @@ public class EmpController {
 	                EmpInfo empInfo = selectedEmpList.get(rowNum);
 	                Row dataRow = sheet.createRow(rowNum + 1);
 
+	                // 각 셀에 사원 정보 데이터 설정
 	                dataRow.createCell(0).setCellValue(empInfo.getEmpNo());
 	                dataRow.createCell(1).setCellValue(empInfo.getEmpName());
 	                dataRow.createCell(2).setCellValue(empInfo.getDeptName());
@@ -353,35 +350,31 @@ public class EmpController {
 	                dataRow.createCell(7).setCellValue(empInfo.getEmployDate());
 	            }
 	            
-	            // 엑셀 데이터를 HTTP 응답 스트림에 작성하여 다운로드
+	            // 생성한 엑셀 데이터를 HTTP 응답 스트림에 작성하여 다운로드
 	            workbook.write(response.getOutputStream());
 	            workbook.close();
-	            
-	            // 리턴값이 null인 경우 현재 페이지에서 계속 머무르도록 함
-	            return null;
 	        } catch (Exception e) {
-	        	// 예외 발생 시 처리	
 	            e.printStackTrace();
+	            // 예외 처리 작업: 예외 발생 시 로깅 또는 사용자에게 알림 등을 수행합니다.
 	        }
-	    } else {
-	    	// downloadExcel과 empNos가 null인 경우, 사원 목록 페이지 렌더링에 필요한 데이터 추가
-	        // 사원 목록 리스트를 전달하여 모델에 추가
-	        List<EmpInfo> selectEmpList = empService.selectEmpList();
-	        log.debug(CC.YE + "EmpController Get empList() selectEmpList: " + selectEmpList + CC.RESET);
-	        
-	        model.addAttribute("selectEmpList", selectEmpList);
-	        
-	        return "/emp/empList"; // 사원 목록 페이지로 이동
 	    }
-	    
-	    return null; // 기본적으로 null을 리턴하도록 수정
 	}
-
+	
+	@GetMapping("/emp/empList")
+	public String empList(Model model) {
+		// 사원 목록 리스트를 전달하여 모델에 추가
+        List<EmpInfo> selectEmpList = empService.selectEmpList();
+        log.debug(CC.YE + "EmpController Get empList() selectEmpList: " + selectEmpList + CC.RESET);
+        
+        model.addAttribute("selectEmpList", selectEmpList);
+        
+        return "/emp/empList"; // 사원 목록 페이지로 이동
+	}
 	
 	// 사원 목록 액션
 	@PostMapping("/emp/empList")
 	public String empList() {
 		log.debug(CC.YE + "EmpController Post empList() : " + CC.RESET);
-	    return "/emp/resetPw";
+	    return "/emp/empList";
 	}
 }
--- edms/src/main/webapp/WEB-INF/view/emp/empList.jsp
+++ edms/src/main/webapp/WEB-INF/view/emp/empList.jsp
@@ -20,19 +20,26 @@
 	        $('.checkbox:checked').each(function() {
 	            selectedEmpNos.push($(this).data('empNo'));
 	        });
-	
+		
+	        // 선택된 사원 번호 배열 출력
+	        console.log("Total selected checkboxes:", $('.checkbox:checked').length);
+		    $('.checkbox:checked').each(function() {
+		        console.log("Data-empNo:", $(this).data('empNo'));
+		    });
+    
 	        if (selectedEmpNos.length > 0) {
 	            // 서버로 선택된 사원의 empNos를 전달하여 엑셀 다운로드 수행
-	            window.location.href = '/emp/empList?empNos=' + selectedEmpNos.join(',');
+	            window.location.href = '/emp/downloadExcel?empNos=' + selectedEmpNos.join(',');
 	        } else {
 	            alert('선택된 사원이 없습니다.');
+	            return false;
 	        }
 	    });
 	    
 	 	// 서버에서 전송한 결과 값 처리
         const urlParams = new URLSearchParams(window.location.search);
         const failParam = urlParams.get('result'); // '?' 제외한 파라미터 이름만 사용
-
+		
         if (failParam == 'success') { // success 파라미터 값이 있을 경우에만 알림 표시
             alert('엑셀 파일 업로드에 성공했습니다.');
         }
@@ -46,15 +53,45 @@
 				$(this).parent().parent().css('background-color', 'yellow'); // 노란색 스타일 추가
 			}
 		});
-        
 	});
 </script>
 </head>
 <body>
-	<button id="excelBtn">엑셀 다운로드</button>
+	<form action="/emp/empList" method="POST">
+		<label for="employmentPeriod">재직기간별:</label>
+        <input type="date" name="startDate" id="startDate">
+        ~
+        <input type="date" name="endDate" id="endDate"><br>
+
+        <label for="employmentStatus">재직상태:</label>
+        <select name="employmentStatus" id="employmentStatus">
+            <option value="재직">재직</option>
+            <option value="퇴직">퇴직</option>
+        </select><br>
+
+        <label for="department">부서별:</label>
+        <select name="department" id="department">
+            <option value="기획추진본부">기획추진본부</option>
+            <option value="경영지원본부">경영지원본부</option>
+            <option value="영업지원본부">영업지원본부</option>
+        </select><br>
+
+        <label for="position">직급별:</label>
+        <select name="position" id="position">
+            <option value="팀장">팀장</option>
+            <option value="팀원">팀원</option>
+        </select><br>
+
+        <label for="gender">성별:</label>
+        <input type="radio" name="gender" value="M">남
+        <input type="radio" name="gender" value="F">여<br>
+		
+		<input type="submit" value="검색">
+    </form>
+
+	<button type="button" id="excelBtn">엑셀 다운로드</button>
 	<table border="1">
 		<tr>
-			<th>엑셀 다운로드</th>
 			<th>비밀번호 초기화</th>
 			<th>사원번호</th>
 			<th>사원명</th>
@@ -67,7 +104,6 @@
 		</tr>
 		<c:forEach var="e" items="${selectEmpList}">
 		<tr>
-			<td><input type="checkbox" class="checkbox" data-empNo="${e.empNo}"></td>
 			<td><button type="button">초기화</button></td>
 			<td>${e.empNo}</td>
 			<td>${e.empName}</td>
@@ -80,5 +116,12 @@
 		</tr>
 		</c:forEach>
 	</table>
+	<c:if test="${currentPage > 1}">
+		<a href="/emp/empList?localName=${localName}&currentPage=${currentPage - 1}">이전</a>
+	</c:if>
+	<p>${currentPage}</p>
+	<c:if test="${currentPage < lastPage}">
+		<a href="/board/boardList?localName=${localName}&currentPage=${currentPage + 1}">다음</a>
+	</c:if>
 </body>
 </html>
