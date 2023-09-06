4358964572a4d3adcbbf6ff8288cdef031465cee
yeeunnnn <133733528+yeeunnnn@users.noreply.github.com>
2023-09-06 14:14:29 +0900

    [김예은] Home 중요 공지사항 리스트 조회

--- edms/src/main/java/com/fit/controller/HomeController.java
+++ edms/src/main/java/com/fit/controller/HomeController.java
@@ -1,18 +1,26 @@
 package com.fit.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.fit.CC;
+import com.fit.service.BoardService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class HomeController {
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping("/home")
 	public String home(HttpSession session, Model model) {
 		
@@ -22,6 +30,9 @@ public class HomeController {
 		int accessLevel = Integer.parseInt(accessLevelStr);
 		String empName = (String)session.getAttribute("empName");		
 		
+		List<Map<String, Object>> selectBoardHome = boardService.selectBoardHome();
+		log.debug(CC.YE + "home컨트롤러 boardList :  " + selectBoardHome + CC.RESET);
+
 log.debug(CC.WOO + "home컨트롤러.세션계층 loginMemberId :  " + loginMemberId + CC.RESET);
 log.debug(CC.WOO + "home컨트롤러.세션계층 accessLevel :  " + accessLevel + CC.RESET);
 log.debug(CC.WOO + "home컨트롤러.세션계층 empName :  " + empName + CC.RESET);
@@ -30,6 +41,7 @@ log.debug(CC.WOO + "home컨트롤러.세션계층 empName :  " + empName + CC.RE
 		model.addAttribute("loginMemberId", loginMemberId);		
 		model.addAttribute("accessLevel", accessLevel);		
 		model.addAttribute("empName", empName);		
+		model.addAttribute("board", selectBoardHome);
 		
 		return "/home";
 	}
--- edms/src/main/webapp/WEB-INF/view/home.jsp
+++ edms/src/main/webapp/WEB-INF/view/home.jsp
@@ -105,11 +105,43 @@
 <!-----------------------------------------------------------------본문 내용 ------------------------------------------------------->    
 <!-- 이 안에 각자 페이지 넣으시면 됩니다 -->
 
+<h4>공지사항</h4>
 
-
-
-
-
+	<table class="table">
+		<tr>
+			<th>공지</th>
+			<th>제목</th>
+			<th>작성자</th>
+			<th>등록일</th>
+		</tr>
+		<!-- 공지가 있을 경우/없을 경우를 분류 -->
+		<c:choose>
+	        <c:when test="${not empty board}">
+	            <c:forEach var="b" items="${board}">
+	                <tr>
+	                    <td>
+	                        <c:choose>
+	                            <c:when test="${b.topExposure == 'Y'}">
+	                                &#128227;
+	                            </c:when>
+	                            <c:otherwise>
+	                                -
+	                            </c:otherwise>
+	                        </c:choose>
+	                    </td>
+	                    <td>${b.boardTitle}</td>
+	                    <td>${b.empName}</td>
+	                    <td>${b.createdate}</td>
+	                </tr>
+	            </c:forEach>
+	        </c:when>
+	        <c:otherwise>
+	            <tr>
+	                <td colspan="4">공지 내용이 없습니다.</td>
+	            </tr>
+	        </c:otherwise>
+    	</c:choose>
+	</table>
 
 <!-----------------------------------------------------------------본문 끝 ------------------------------------------------------->          
 
